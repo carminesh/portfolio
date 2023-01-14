f0206a6ed92d59d14cae8d03d27d58bd3bb0879c
carminesh <carmineshoot5@outlook.it>
2023-01-14 23:50:12 +0100

    change(MainHero): main hero layout responsive layout updated

--- src/layouts/MainHero.tsx
+++ src/layouts/MainHero.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react';
 import ActionButton from '../components/ActionButton';
 import mailIcon from '../assets/mailIcon.svg';
 import downloadIcon from '../assets/downloadIcon.svg';
+import computerIcon from '../assets/computerIcon.svg';
 import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -11,11 +12,11 @@ function MainHero() {
     }, []);
 
     return (
-        <div className="w-screen h-screen bg-slate-50 pt-6">
-            <section className="w-5/6 h-auto px-10 py-10 flex-col phone:px-4">
-                <h2 className="text-5xl font-normal text-black mb-2">Hi There,</h2>
-                <h1 className="text-7xl font-medium mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-gradient-1 to-purple-gradient-2 phone:text-6xl">I'm Carmine</h1>
-                <p className="text-xl font-normal text-light-gray phone:text-base">
+        <div className="bg-slate-50 w-screen h-screen flex-row items-center px-10 pt-10 md:flex md:pb-40 lg:px-28">
+            <section className="flex-1 h-auto flex-col phone:px-4">
+                <h2 className="text-4xl sm:text-5xl font-normal text-black mb-2">Hi There,</h2>
+                <h1 className="text-5xl sm:text-7xl font-medium mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-gradient-1 to-purple-gradient-2">I'm Carmine</h1>
+                <p className="text-l sm:text-xl font-normal text-light-gray phone:text-base">
                     A computer science graduate. <br></br>Skilled in both individual and team environments and constantly seeking out new opportunities to learn and grow. <br></br>Committed to
                     delivering results and making a positive impact.
                 </p>
@@ -40,6 +41,8 @@ function MainHero() {
                     </a>
                 </div>
             </section>
+
+            <img src={computerIcon} alt="computer icon" className="mt-14 md:flex-initial md:w-2/4 lg:w-2/5" />
         </div>
     );
 }
