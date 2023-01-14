0fb97fa755de08f2aedd0599c63bf8e3ff1b8157
carminesh <carmineshoot5@outlook.it>
2023-01-14 14:35:26 +0100

    change(tailwind.config): new config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      'phone': {'min': '375px', 'max': '500px'},
+    },
+    extend: {
+      colors: {
+        'purple-gradient-1': '#B38EFF',
+        'purple-gradient-2': '#858CFF',
+        'light-gray': '#585858'
+      },
+
+    },
   },
   plugins: [],
 }
