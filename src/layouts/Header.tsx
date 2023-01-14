e88ab7a420b9e1f18cdeae0168cd6b9d35440e64
carminesh <carmineshoot5@outlook.it>
2023-01-14 23:49:37 +0100

    change(Header): minor update to padding

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
+import themeIcon from '../assets/themeIcon.svg';
 
 const Header: React.FC = () => {
     return (
-        <nav className="flex flex-row justify-between bg-slate-50 h-28 w-screen px-10 py-10">
+        <nav className="flex flex-row justify-between bg-slate-50 h-28 w-screen px-12 py-10">
             <img src={logo} alt={'button icon'} className="icon" />
-            <img src={logo} alt={'button icon'} className="icon" />
+            <img src={themeIcon} alt={'button icon'} className="icon" />
         </nav>
     );
 };
