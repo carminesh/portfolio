635e9b74ef35d034c8fc1112e0b3f919c3864c85
carminesh <carmineshoot5@outlook.it>
2023-01-15 19:19:28 +0100

    change(MainHero.tsx): added the two new section - Education && Experience

--- src/layouts/MainHero.tsx
+++ src/layouts/MainHero.tsx
@@ -6,13 +6,13 @@ import computerIcon from '../assets/computerIcon.svg';
 import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function MainHero() {
+const MainHero: React.FC = () => {
     const contactAction = useCallback(() => {
         console.log('CONTACT');
     }, []);
 
     return (
-        <div className="bg-slate-50 w-screen h-screen flex-row items-center px-10 pt-10 md:flex md:pb-40 lg:px-28">
+        <div className="bg-slate-50 w-auto h-auto flex-row items-center pl-6 pr-10 pt-10 md:flex md:pb-40 lg:px-28">
             <section className="flex-1 h-auto flex-col phone:px-4">
                 <h2 className="text-4xl sm:text-5xl font-normal text-black mb-2">Hi There,</h2>
                 <h1 className="text-5xl sm:text-7xl font-medium mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-gradient-1 to-purple-gradient-2">I'm Carmine</h1>
@@ -45,6 +45,6 @@ function MainHero() {
             <img src={computerIcon} alt="computer icon" className="mt-14 md:flex-initial md:w-2/4 lg:w-2/5" />
         </div>
     );
-}
+};
 
 export default MainHero;
