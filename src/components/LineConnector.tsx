a44d862f012cf6cb8afeb4f618ab18a528202d29
carminesh <carmineshoot5@outlook.it>
2023-01-16 20:38:50 +0100

    new(LineConnector.tsx): added new component for line story

---
+++ src/components/LineConnector.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LineConnector: React.FC = () => {
+    return (
+        <div className="flex flex-col flex-inital h-[60px] w-[4px] bg-purple-gradient-1 mr-6 tems-center justify-between">
+            <div className="w-[4px] h-[10px] bg-slate-50"></div>
+            <div className="w-[4px] h-[10px] bg-slate-50"></div>
+            <div className="w-[4px] h-[10px] bg-slate-50"></div>
+        </div>
+    );
+};
+
+export default LineConnector;
