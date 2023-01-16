11b4446493af5c5403883c954c0c7a006c7fc91a
carminesh <carmineshoot5@outlook.it>
2023-01-16 20:39:24 +0100

    chnage(Education.tsx): minor refactoring

--- src/layouts/Education.tsx
+++ src/layouts/Education.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo, useState } from 'react';
+import LineConnector from '../components/LineConnector';
 import ShowcaseItem from '../components/ShowcaseItem';
 import { ExplainingModel } from '../models/ExplainingModel';
 
