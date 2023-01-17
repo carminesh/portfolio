08505af55ad70f5b8bd26fd96f8d71e4c3bc4c58
carminesh <carmineshoot5@outlook.it>
2023-01-17 22:08:41 +0100

    change(index): provided the store for the entire App

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import '../src/styles/index.css';
 import App from './screens/App';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
