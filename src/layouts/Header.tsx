4157ad5ee0b347872cf6d92b3d7b8c9b7ccebca3
carminesh <carmineshoot5@outlook.it>
2023-01-18 21:52:11 +0100

    chnage(Header): minor refactoring

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { switchTheme, getSelectedTheme } from '../store/slices/ThemeSlice';
-import { useEffect } from 'react';
 import PersonalLogo from '../assets/PersonalLogo';
 import ThemeIcon from '../assets/ThemeIcon';
 
