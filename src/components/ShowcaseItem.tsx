5011966baa6d3c6838680f841078776686c41354
carminesh <carmineshoot5@outlook.it>
2023-01-18 21:50:56 +0100

    change(ShowcaseItem): fixed width on bigger devices

--- src/components/ShowcaseItem.tsx
+++ src/components/ShowcaseItem.tsx
@@ -7,7 +7,7 @@ interface ShowcaseItemProps {
 
 const ShowcaseItem: React.FC<ShowcaseItemProps> = ({ item }) => {
     return (
-        <div className="my-10 lg:w-2/4">
+        <div className="my-10 lg:w-3/4">
             <p className="font-light text-light-gray pt-1 dark:text-theme-font-secondary">{item.date}</p>
             <h2 className="font-semibold text-black text-2xl pt-1 dark:text-theme-font-secondary">{item.title}</h2>
             {item.subtitle && <h2 className="font-semibold text-xl text-purple-gradient-1 pt-1">{item.subtitle}</h2>}
