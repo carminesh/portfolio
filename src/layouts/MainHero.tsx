6a2899273d64f2e670481a36dbeb0252fafb5d6f
carminesh <carmineshoot5@outlook.it>
2023-01-18 21:52:46 +0100

    change(MainHeroSection): image and width manipolation

--- src/layouts/MainHero.tsx
+++ src/layouts/MainHero.tsx
@@ -5,6 +5,7 @@ import downloadIcon from '../assets/downloadIcon.svg';
 import computerIcon from '../assets/computerIcon.svg';
 import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../styles/animation.css';
 
 const MainHero: React.FC = () => {
     const contactAction = useCallback(() => {
@@ -12,8 +13,8 @@ const MainHero: React.FC = () => {
     }, []);
 
     return (
-        <div className="bg-slate-50 w-auto h-auto flex-row items-center pl-6 pr-10 pt-10 md:flex md:pb-40 lg:px-28 dark:bg-theme-dark-bg">
-            <section className="flex-1 h-auto flex-col phone:px-4">
+        <div className="bg-slate-50 w-auto h-auto flex-row items-center pl-6 pr-10 pt-10 md:flex md:pb-40  dark:bg-theme-dark-bg">
+            <section className="flex-1 h-auto flex-col ">
                 <h2 className="text-4xl sm:text-5xl font-normal text-black mb-2 dark:text-theme-font-secondary">Hi There,</h2>
                 <h1 className="text-5xl sm:text-7xl font-medium mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-gradient-1 to-purple-gradient-2">I'm Carmine</h1>
                 <p className="text-l sm:text-xl font-normal text-light-gray phone:text-base dark:text-theme-font-secondary">
@@ -21,7 +22,7 @@ const MainHero: React.FC = () => {
                     delivering results and making a positive impact.
                 </p>
 
-                <div className="mt-5 flex flex-row justify-between w-80">
+                <div className="mt-5 flex flex-row justify-between w-[328px]">
                     <ActionButton
                         icon={mailIcon}
                         text={'Contact Me'}
@@ -34,15 +35,20 @@ const MainHero: React.FC = () => {
 
                 <div className="flex flex-rowitems-center mt-5">
                     <a href="https://www.linkedin.com/in/carmine-fabbri-0a1b24181/">
-                        <FontAwesomeIcon icon={faLinkedin} size="3x" className="text-black hover:text-purple-gradient-1 dark:text-theme-font-secondary" />
+                        <FontAwesomeIcon icon={faLinkedin} size="3x" className="text-black dark:text-theme-font-secondary dark:hover:text-purple-gradient-1 hover:text-purple-gradient-1" />
                     </a>
                     <a href="https://github.com/carminesh">
-                        <FontAwesomeIcon icon={faSquareGithub} size="3x" style={{ marginLeft: 10 }} className="text-black hover:text-purple-gradient-1 dark:text-theme-font-secondary" />
+                        <FontAwesomeIcon
+                            icon={faSquareGithub}
+                            size="3x"
+                            style={{ marginLeft: 10 }}
+                            className="text-black dark:text-theme-font-secondary dark:hover:text-purple-gradient-1 hover:text-purple-gradient-1"
+                        />
                     </a>
                 </div>
             </section>
 
-            <img src={computerIcon} alt="computer icon" className="mt-14 md:flex-initial md:w-2/4 lg:w-2/5" />
+            <img className="computerIcon" src={require('../assets/computerIconImg.png')} alt="computer icon" />
         </div>
     );
 };
