09229e63b476c311377dec8f240f08679d998148
carminesh <carmineshoot5@outlook.it>
2023-01-20 11:28:56 +0100

    change(ShowCaseItem): fixed position

--- src/components/ShowcaseItem.tsx
+++ src/components/ShowcaseItem.tsx
@@ -11,7 +11,7 @@ const ShowcaseItem: React.FC<ShowcaseItemProps> = ({ item }) => {
             <p className="font-light text-light-gray pt-1 dark:text-theme-font-secondary">{item.date}</p>
             <h2 className="font-semibold text-black text-2xl pt-1 dark:text-theme-font-secondary">{item.title}</h2>
             {item.subtitle && <h2 className="font-semibold text-xl text-purple-gradient-1 pt-1">{item.subtitle}</h2>}
-            <p className="pt-1 font-light">{item.description}</p>
+            <p className="pt-1 font-regular">{item.description}</p>
         </div>
     );
 };
