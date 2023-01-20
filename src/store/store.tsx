86ae48e0b809f16bf4bb2517159bebaa88b6609c
carminesh <carmineshoot5@outlook.it>
2023-01-20 11:30:55 +0100

    change(store.tsx): major changes applied to the store because of persist gate inegration

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,11 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { ThemeSlice } from './slices/ThemeSlice';
-// ...
-const store = configureStore({
-    reducer: {
-        themeReducer: ThemeSlice.reducer,
-    },
-});
-export type RootState = ReturnType<typeof store.getState>;
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const rootReducer = combineReducers({
+    themeReducer: ThemeSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
+export const persistor = persistStore(store);
